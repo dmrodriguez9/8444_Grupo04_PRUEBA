4b931ad149b406d861cb4eda10ff0e59acefdfae
dannyRodriguez9 <dmrodriguez9@espe.edu.ec>
2022-12-22 04:37:26 -0500

    modificaciones

--- Recursos/js/funcionesEjemplos.js
+++ Recursos/js/funcionesEjemplos.js
@@ -104,3 +104,92 @@ function añadirNodos()
   const child = document.getElementById("p1");
   element.insertBefore(para,child);
 }
+
+/*jQuery HTML*/
+  /*jQuery Get*/
+function agregarJQueryGet1()
+{
+  $(document).ready(function(){
+    $("#btn1").click(function(){
+      alert("Text: " + $("#test").text());
+    });
+  });
+}
+
+function agregarJQueryGet2()
+{
+  $(document).ready(function(){
+    $("#btn2").click(function(){
+      alert("HTML: " + $("#test").html());
+    });
+  });
+}
+
+function agregarJQueryGetMetodoVal()
+{
+  $(document).ready(function(){
+    $("button").click(function(){
+      alert("Valor: " + $("#texto").val());
+    });
+  });
+}
+
+ /*jQuery Set*/
+ function agregarJQuerySet()
+ {
+  $(document).ready(function(){
+    $("#btn1").click(function(){
+      $("#test1").text("Hola Mundo!");
+    });
+    $("#btn2").click(function(){
+      $("#test2").html("<b>Aplicaciones de Tecnolgías Web!</b>");
+    });
+    $("#btn3").click(function(){
+      $("#test3").val("Nathaly Maribel");
+    });
+  });
+ }
+ 
+
+ function agregarJQuerySetFDevolucion()
+ {
+  $(document).ready(function(){
+    $("#btn4").click(function(){
+      $("#parrafo1").text(function(i, origText){
+        return "Viejo texto: " + origText + " Nuevo texto: Hola mundo! (index: " + i + ")"; 
+      });
+    });
+  
+    $("#btn5").click(function(){
+      $("#parrafo2").html(function(i, origText){
+        return "Viejo html: " + origText + " Nuevo html: Hola <b>mundo!</b> (index: " + i + ")"; 
+      });
+    });
+  });
+ }
+
+  /*jQuery Add*/
+  function agregarJQueryAddFinal()
+  {
+    $(document).ready(function(){
+      $("#btn1").click(function(){
+        $("p").append(" <b>Texto adjuntado al final</b>.");
+      });
+    
+      $("#btn2").click(function(){
+        $("ol").append("<li>Elemento adjuntado al final</li>");
+      });
+    });
+  }
+
+  function agregarJQueryAddPrincipio()
+  {
+    $(document).ready(function(){
+      $("#btn3").click(function(){
+        $("p").prepend("<b>Se agrego el texto al principio</b>. ");
+      });
+      $("#btn4").click(function(){
+        $("ul").prepend("<li>Se agrego un elemento al principio</li>");
+      });
+    });
+  }
